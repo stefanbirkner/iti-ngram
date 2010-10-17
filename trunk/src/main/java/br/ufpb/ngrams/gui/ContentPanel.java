ddb806bbeb03ae3403bd80acb2703e8013c58c48
StefanFriedrichBirkner <StefanFriedrichBirkner@65b74166-b006-11dd-b2cc-8fa0b79a9668>
2010-10-17 13:34:41 +0000

    simplified the ContentPanel

--- trunk/src/main/java/br/ufpb/ngrams/gui/ContentPanel.java
+++ trunk/src/main/java/br/ufpb/ngrams/gui/ContentPanel.java
@@ -8,45 +8,35 @@ import javax.swing.JTextArea;
 
 public class ContentPanel extends JPanel
 {
-	private static final long serialVersionUID = 1093998575730432278L;
-	
-	private static ContentPanel instance = null;
-	private JTextArea textArea;
-	private JScrollPane scrollPane;
-	
-	private ContentPanel() {
-		this.init();
-	}
-	
-	public static ContentPanel getInstance()
-	{
-		if (instance == null) {
-			instance = new ContentPanel();
-		}
-		return instance;
-	}
-	
-	private void init()
-	{
-		this.setLayout(new GridLayout(1,1));
-		this.add(this.getScrollPane());
-	}
-	
-	public JScrollPane getScrollPane()
-	{
-		if (scrollPane == null)
-		{
-			scrollPane = new JScrollPane();
-			scrollPane.setViewportView(getTextArea());
-		}
-		return scrollPane;
-	}
-	
-	public JTextArea getTextArea()
-	{
-		if (textArea == null) {
-			textArea = new JTextArea();
-		}
-		return textArea;
-	}
+  private static final long serialVersionUID = 1093998575730432279L;
+
+  private static ContentPanel instance = null;
+  private final JTextArea textArea = new JTextArea();
+
+  private ContentPanel()
+  {
+    setLayout(new GridLayout(1, 1));
+    createScrollPane();
+  }
+
+  public static ContentPanel getInstance()
+  {
+    if (instance == null)
+    {
+      instance = new ContentPanel();
+    }
+    return instance;
+  }
+
+  private void createScrollPane()
+  {
+    JScrollPane scrollPane = new JScrollPane();
+    scrollPane.setViewportView(textArea);
+    add(scrollPane);
+  }
+
+  public JTextArea getTextArea()
+  {
+    return textArea;
+  }
 }
