22f1cb0f42596a2c647e637fcdfb2b8fea614d53
StefanFriedrichBirkner <StefanFriedrichBirkner@65b74166-b006-11dd-b2cc-8fa0b79a9668>
2010-12-16 19:36:22 +0000

    set EXIT_ON_CLOSE as default close operation

--- trunk/src/main/java/br/ufpb/ngrams/ItiNgram.java
+++ trunk/src/main/java/br/ufpb/ngrams/ItiNgram.java
@@ -1,5 +1,6 @@
 package br.ufpb.ngrams;
 
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
 import br.ufpb.ngrams.gui.MainFrame;
 
 public class ItiNgram
@@ -8,6 +9,7 @@ public class ItiNgram
   {
     String title = String.format("%s - %s", MainProperties.MAINWINDOW_TITLE, MainProperties.VERSION);
     MainFrame f = new MainFrame(title);
+    f.setDefaultCloseOperation(EXIT_ON_CLOSE); 
     f.setVisible(true);
   }
 }
