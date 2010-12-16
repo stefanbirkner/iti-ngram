4742df69935520e943d2ac67f0b41a3accc6ec04
StefanFriedrichBirkner <StefanFriedrichBirkner@65b74166-b006-11dd-b2cc-8fa0b79a9668>
2010-12-16 19:55:45 +0000

    removed getters and setters from Attributes

--- src/main/java/br/ufpb/ngrams/Attributes.java
+++ src/main/java/br/ufpb/ngrams/Attributes.java
@@ -5,43 +5,25 @@ import java.util.List;
 
 public class Attributes
 {
-	
-	private List<Character> ignoredCharacters = new ArrayList<Character>();
+  public List<Character> ignoredCharacters = new ArrayList<Character>();
+  public boolean ignoreDigits;
+  public boolean ignoreLetters;
+  public boolean ignoreWhitespaces;
+  public boolean ignoreSymbols;
+  public boolean mergeWhitespaces;
+  public boolean ignoreConsecutive;
+  public boolean convertDowncase;
 
-	private boolean ignoreDigits;
-	private boolean ignoreLetters;
-	private boolean ignoreWhitespaces;
-	private boolean ignoreSymbols;
-	private boolean mergeWhitespaces;
-	private boolean ignoreConsecutive;
-	private boolean convertDowncase;
-	
-	public void setAttributes(boolean ignoreDigits, boolean ignoreLetters, boolean ignoreWhitespaces, boolean ignoreSymbols, boolean mergeWhiteSpaces, boolean mergeConsecutive, boolean convertDownCase)
-	{
-		this.ignoreDigits = ignoreDigits;
-		this.ignoreLetters = ignoreLetters;
-		this.ignoreWhitespaces = ignoreWhitespaces;
-		this.ignoreSymbols = ignoreSymbols;
-		this.mergeWhitespaces = mergeWhiteSpaces;
-		this.ignoreConsecutive = mergeConsecutive;
-		this.convertDowncase = convertDownCase;
-	}
-
-	public List<Character> getIgnoredCharacters() { return ignoredCharacters; }
-	public boolean isIgnoreLetters() { return ignoreLetters; }
-	public boolean isIgnoreDigits() { return ignoreDigits; }
-	public boolean isIgnoreSymbols() { return ignoreSymbols; }
-	public boolean isIgnoreWhitespaces() { return ignoreWhitespaces; }
-	public boolean isMergeWhitespaces() { return mergeWhitespaces; }
-	public boolean isIgnoreConsecutive() { return ignoreConsecutive; }
-	public boolean isConvertDowncase() { return convertDowncase; }
-
-	public void setIgnoredCharacters(List<Character> ignoredCharacters) { this.ignoredCharacters = ignoredCharacters; }
-	public void setIgnoreLetters(boolean ignoreLetters) { this.ignoreLetters = ignoreLetters; }
-	public void setIgnoreDigits(boolean ignoreDigits) { this.ignoreDigits = ignoreDigits; }
-	public void setIgnoreWhitespaces(boolean ignoreWhiteSpaces) { this.ignoreWhitespaces = ignoreWhiteSpaces; }
-	public void setIgnoreSymbols(boolean ignoreSymbols) { this.ignoreSymbols = ignoreSymbols; }
-	public void setMergeWhitespaces(boolean mergeWhitespaces) { this.mergeWhitespaces = mergeWhitespaces; }
-	public void setIgnoreConsecutive(boolean ignoreConsecutive) { this.ignoreConsecutive = ignoreConsecutive; }
-	public void setConvertDowncase(boolean convertDownCase) { this.convertDowncase = convertDownCase; }
+  public void setAttributes(boolean ignoreDigits, boolean ignoreLetters, boolean ignoreWhitespaces,
+      boolean ignoreSymbols, boolean mergeWhiteSpaces, boolean mergeConsecutive,
+      boolean convertDownCase)
+  {
+    this.ignoreDigits = ignoreDigits;
+    this.ignoreLetters = ignoreLetters;
+    this.ignoreWhitespaces = ignoreWhitespaces;
+    this.ignoreSymbols = ignoreSymbols;
+    this.mergeWhitespaces = mergeWhiteSpaces;
+    this.ignoreConsecutive = mergeConsecutive;
+    this.convertDowncase = convertDownCase;
+  }
 }
